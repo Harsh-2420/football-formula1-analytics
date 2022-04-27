e8e7e03bf9bcc567951fe0e637d87782814d5675
Harsh-2420 <harsh2420@gmail.com>
2022-04-27 19:21:36 +0530

    added events

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify, request, json
+from flask import Flask, jsonify, request, json, session
+from flask_session import Session
 from flask_cors import CORS
 from random import randrange
 from flask_sqlalchemy import SQLAlchemy
 import fastf1
 import requests
+import datetime
 
 app = Flask(__name__)
+# app.config["SESSION_PERMANENT"] = True
+SESSION_TYPE = "filesystem"
+app.config.from_object(__name__)
+Session(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 app.config['SQLALCHEMY_BINDS'] = {
     "year": "sqlite:///year.db",
@@ -15,13 +21,14 @@ app.config['SQLALCHEMY_BINDS'] = {
 }
 db = SQLAlchemy(app)
 CORS(app)
-# fastf1.Cache.enable_cache('../f1_cache')
+fastf1.Cache.enable_cache('../f1_cache')
+# global g_selected_year
 
 
 class Year(db.Model):
     __bind_key__ = 'year'
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(80), unique=True)
+    content = db.Column(db.String(80), unique=False)
 
     def __str__(self):
         return f'{self.id} {self.content}'
@@ -73,6 +80,19 @@ def serializer(todo):
 
 @app.route('/api/getyear', methods=['GET'])
 def getyear():
+    try:
+        # Year.query.filter_by(id=2).first()
+        Year.query.all()
+        db.drop_all(bind='year')
+        db.create_all(bind='year')
+    except:
+        db.create_all(bind='year')
+        db.drop_all(bind='year')
+        db.create_all(bind='year')
+
+    for year in range(2018, datetime.datetime.now().year+1):
+        db.session.add(Year(content=year))
+    db.session.commit()
     return jsonify([*map(serializer, Year.query.all())])
 
 
@@ -94,9 +114,11 @@ def getdriver():
 @app.route('/api/selectyear', methods=['POST'])
 def selectyear():
     if request.method == 'POST':
-        year = request.get_json(force=True)
+        selected_year = request.get_json(force=True)
+        session['selected_year'] = selected_year
+        # app.logger.info(type(session_selection))
         unformatted_names = requests.get(
-            "http://ergast.com/api/f1/{}/circuits".format(year)).text.split('<CircuitName>')[1:]
+            "http://ergast.com/api/f1/{}/circuits".format(selected_year)).text.split('<CircuitName>')[1:]
         circuit_list = [x.split('</CircuitName>')[0]
                         for x in unformatted_names]
         db.create_all(bind='race')
@@ -105,25 +127,33 @@ def selectyear():
         for race in circuit_list:
             db.session.add(Race(content=race))
         db.session.commit()
-        return {"201": year}
+        return {"201": selected_year}
 
 
 @app.route('/api/selectrace', methods=['POST'])
 def selectrace():
     if request.method == 'POST':
-        race = request.get_json(force=True)
-        # db.session.add(Race(content='Singapore'))
-        # db.session.commit()
-        app.logger.info(str("#######################" + race))
-        return {"201": race}
+        selected_race = request.get_json(force=True)
+        if not 'selected_year' in session:
+            session['selected_year'] = 2021
+        selected_year = session.get('selected_year', None)
+        # app.logger.info()
+        f1_session = fastf1.get_event(int(selected_year), selected_race)
+        f1_session_list = [f1_session['Session1'], f1_session['Session2'],
+                           f1_session['Session3'], f1_session['Session4'], f1_session['Session5']]
+        db.create_all(bind='event')
+        db.drop_all(bind='event')
+        db.create_all(bind='event')
+        for event in f1_session_list:
+            db.session.add(Event(content=event))
+        db.session.commit()
+        return {"201": selected_race}
 
 
 @app.route('/api/selectevent', methods=['POST'])
 def selectevent():
     if request.method == 'POST':
         event = request.get_json(force=True)
-        # db.session.add(Event(content=request_data))
-        # db.session.commit()
         return {"201": event}
 
 
