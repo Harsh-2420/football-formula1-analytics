36f5a74249a46e6b77db48ce2219a9172d40a977
Harsh-2420 <harsh2420@gmail.com>
2022-04-28 12:38:57 +0530

    testing @mui components for frontent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css"
+import * as React from "react"
 import { TodoPage } from "./Pages/TodoPage"
 import { DropdownPage } from "./Pages/DropdownPage"
 import "bootstrap/dist/css/bootstrap.min.css"
--- frontend/src/Components/TodoItem.js
+++
@@ -1,8 +0,0 @@
-export const TodoItem = ({ todo, onEditClick, onDeleteClick }) => {
-    return (
-        <li>
-            {todo.content}
-            <button onClick={() => onDeleteClick(todo.id)}>Delete</button>
-        </li>
-    )
-}
